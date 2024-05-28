c1bda5f202affc983fb4946a29e150b006194880
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-28 22:31:59 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function(x, y) {
+    return x / y;
+  }
+  
+  console.log(divide(2000, 100)); 
+  
+
+  const square = (x) => x * x;
+
+  console.log(square(5, 5)); 
+  
+
+  const add = (y, j) =>  y + j;
+ 
+console.log(add(6, 8))
+
+
